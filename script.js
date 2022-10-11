a3da35ad766d0623479800ebdeb7170bfde031a1
cjvuoksi <cjvuoksi@gmail.com>
2022-10-11 21:11:29 +0000

    output DOM

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 import {destinations, ferryNames} from "./destinations.js"; 
 var departure = document.getElementById("departure"); 
 var destination = document.getElementById("destination");  
+var jsOut = document.getElementById("schedule"); 
 
 window.onload = (event) => {
     departure.selectedIndex = 0; 
@@ -31,13 +32,58 @@ function formHandler(event) {
   getApi(); 
 }
 
+
 function getApi() {
-    let url = "https://www.bcferriesapi.ca/api/" + departure.options[departure.selectedIndex].value 
-    url = destination.options[destination.selectedIndex].value ? "/" + destination.options[destination.selectedIndex].value : ""; 
+    let from = departure.options[departure.selectedIndex].value; 
+    let to = destination.options[destination.selectedIndex].value; 
+    // let url = "https://www.bcferriesapi.ca/api/" + departure.options[departure.selectedIndex].value; 
+    // url += destination.options[destination.selectedIndex].value ? "/" + destination.options[destination.selectedIndex].value : "/";
+    let url = 'https://www.bcferriesapi.ca/api/'
     fetch(url)
         .then(function(response) {
-            return response.json();
-        }).then(function(json) {
-            console.log(json);
-        });
+            console.log(response); 
+            
+            return response.json(); 
+        })
+        .then((json) => {
+            console.log(json); 
+            jsOut.replaceChildren("");  
+            let schedule = json.schedule; 
+            if (schedule.hasOwnProperty(from)) {
+                if (schedule.from.hasOwnProperty(to)) {
+                    departDOM(schedule[from], from, to); 
+                }
+                else {
+                    departDOM(schedule[from], from); 
+                }
+            }
+            else {
+                for (let i of Object.keys(schedule)) {
+                    departDOM(schedule[i], i); 
+                }
+            }
+        }); 
+}   
+
+function departDOM(dpt, name, to = null) {
+    debugger
+    let div = document.createElement("div"); 
+    div.classList.add("departure"); 
+    div.innerHTML = getName(name);  
+    for (let i of Object.keys(dpt)) {
+        let temp = document.createElement("p"); 
+        temp.innerHTML = getName(i); 
+        div.appendChild(temp); 
+    }
+    jsOut.appendChild(div); 
 }
+
+function getName(ferryAbbreviation) {
+    return ferryNames[ferryAbbreviation]; 
+}
+
+const fetchInit = {
+    method: 'GET',
+    redirect: 'follow'
+}
+
